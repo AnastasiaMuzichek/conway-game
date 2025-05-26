a3a6267083f9ac1885ade5b9d4be23cc3748c58f
Anastasia <anastasiamyzichek1997@gmail.com>
2025-05-26 20:32:33 +0400

    Change

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [vue()],
     build: {
         assetsDir: './',
+        outDir: 'root',
         rollupOptions: {
             output: {
                 entryFileNames: 'main.js',
